1974dcd6069446371df5e2a40057779959bd6e7b
grungger <123024860+grungger@users.noreply.github.com>
2023-04-13 14:58:31 +0200

    config file

--- MRS_PSF_subtraction_FS23/Data_cube_input/moduletester.py
+++ MRS_PSF_subtraction_FS23/Data_cube_input/moduletester.py
@@ -2,15 +2,55 @@
 testfile to check any modules
 """
 import sys
+import os
+import pdb
 
 #########################
 # User specific path
 sys.path.append('/Users/Gian/Documents/GitHub/Pynpoint')
 #########################
 
+sys.path.append("/Users/Gian/Documents/Github/Pynpoint_ifs/background_files")
+sys.path.append('/Users/Gian/Documents/GitHub/Pynpoint')
+
+#from spectres import spectres
+from ifuframeselection import SelectWavelengthRangeModule
+from ifubadpixel import NanFilterModule
+from ifucentering_test import IFUAlignCubesModule
+from ifupsfpreparation import IFUStellarSpectrumModule
+from ifupsfsubtraction import IFUPSFSubtractionModule
+from ifuresizing import FoldingModule
+from ifustacksubset import CrossCorrelationPreparationModule
+from ifucrosscorrelation import CrossCorrelationModule
+from ifupcasubtraction import IFUResidualsPCAModule
+from ifuresizing import UnfoldingModule
+
+from pynpoint import Pypeline, WavelengthReadingModule, FitsReadingModule, \
+                    FitCenterModule, RemoveLinesModule, StackCubesModule, \
+                    BadPixelSigmaFilterModule, ParangReadingModule, \
+                    AddLinesModule, FitsWritingModule, TextWritingModule, \
+                    PrimaryHDUCombiner
+
+
 from scratch_primaryhducombiner import hducollapser
 
-file_path = '/Users/Gian/Documents/GitHub/Pynpoint_testcode/Data/input/Level3_ch1-long_s3d.fits'
-out_path = '/Users/Gian/Documents/GitHub/Pynpoint_testcode/Data/output'
+# pdb.set_trace()
 
-headr = hducollapser(file_path,out_path)
+file_path = '/Users/Gian/Documents/GitHub/Pynpoint_testcode/Data/input/Level3_ch1-long_s3d.fits'
+in_path = '/Users/Gian/Documents/JWST_Central-Database/cubes_obs3'
+work_path = '/Users/Gian/Documents/GitHub/Pynpoint_testcode/MRS_PSF_subtraction_FS23/Data_cube_input'
+out_path = '/Users/Gian/Documents/GitHub/Pynpoint_testcode/Data/output/cubes_obs3'
+
+# headr = hducollapser(file_path,out_path)
+
+pipeline = Pypeline(work_path, in_path, out_path)
+
+Collapser = PrimaryHDUCombiner(name_in = "collagen", 
+                               input_dir = in_path,
+                               output_dir = out_path,
+                               image_tag = "im",
+                               overwrite=True)
+
+pipeline.add_module(Collapser)
+pipeline.run_module("collagen")
+
